d2b468dc714d3cbe7f94216602c0c4227ba3d9c3
tsutsui <shoppa99.nanone@hotmail.co.jp>
2021-01-03 17:05:43 +0900

    update set_spins

--- src/set_spins.jl
+++ src/set_spins.jl
@@ -1,5 +1,5 @@
 """
-    set_spins(N, siteindeces, σs)
+    set_spins(N, siteindeces::Vector{Int}, σs)
 
 Return a list of Pauli matrices.
 
@@ -15,11 +15,32 @@ julia> set_spins(4, [2, 3], [σᶻ, σˣ]) == [σ⁰, σᶻ, σˣ, σ⁰]
 true
 ```
 """
-function set_spins(N, siteindeces, σs)
+function set_spins(N, siteindeces::Vector{Int}, σs)
     siteindeces = pbc(siteindeces, N)
     list_mats = fill(σ⁰, N)
     for (i, σ) in zip(siteindeces, σs)
             list_mats[i] = σ
     end
+    return list_mats
+end
+
+"""
+    set_spins(N, siteindeces::CartesianIndex, σs)
+
+Return a list of Pauli matrices, where siteindeces is the row and column number
+of an adjacency matrix.
+
+# Examples
+```julia-repl
+julia> set_spins(4, CartesianIndex(2, 3), [σᶻ, σˣ]) == [σ⁰, σᶻ, σˣ, σ⁰]
+true
+```
+"""
+function set_spins(N, siteindeces::CartesianIndex, σs)
+    @assert length(siteindeces) == 2 "len(siteindeces) must be 2."
+    @assert length(σs) == 2 "len(σs) must be 2."
+    list_mats = fill(σ⁰, N)
+    list_mats[siteindeces[1]] = σs[1]
+    list_mats[siteindeces[2]] = σs[2]
     return list_mats
 end
--- test/test_set_spins.jl
+++ test/test_set_spins.jl
@@ -1,5 +1,11 @@
 @testset "set spins" begin
+    # Use a list of integers.
     @test set_spins(3, [2], [σᶻ]) == [σ⁰, σᶻ, σ⁰]
     @test set_spins(4, [2], [σᶻ]) == [σ⁰, σᶻ, σ⁰, σ⁰]
     @test set_spins(4, [2, 3], [σᶻ, σˣ]) == [σ⁰, σᶻ, σˣ, σ⁰]
+
+    # Use a CartesianIndex.
+    @test set_spins(4, CartesianIndex(2, 3), [σᶻ, σˣ]) == [σ⁰, σᶻ, σˣ, σ⁰]
+    @test_throws AssertionError set_spins(4, CartesianIndex(2), [σᶻ, σˣ])
+    @test_throws AssertionError set_spins(4, CartesianIndex(2,3), [σᶻ])
 end
